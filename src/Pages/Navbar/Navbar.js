fe0d4aae051b5c4074500972826e891f74e62f0f
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-02-28 18:15:23 +0600

    navbar icon added

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaHome, FaSignInAlt, FaUserPlus } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
@@ -11,18 +12,18 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to='/home'>Home</Link></li>
-                            <li><Link to='/login'>Log-In</Link></li>
-                            <li><Link to='/register'>Register</Link></li>
+                            <li><Link to='/home'><FaHome></FaHome>Home</Link></li>
+                            <li><Link to='/login'><FaSignInAlt></FaSignInAlt>Log-In</Link></li>
+                            <li><Link to='/register'><FaUserPlus></FaUserPlus>Register</Link></li>
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><Link to='/home'>Home</Link></li>
-                        <li><Link to='/login'>Log-In</Link></li>
-                        <li><Link to='/register'>Register</Link></li>
+                        <li><Link to='/home'><FaHome></FaHome>Home</Link></li>
+                        <li><Link to='/login'><FaSignInAlt></FaSignInAlt>Log-In</Link></li>
+                        <li><Link to='/register'><FaUserPlus></FaUserPlus>Register</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
